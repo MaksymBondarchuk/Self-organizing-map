4ea79b69757535cde4c6cbdecc1e709853a8b982
MaxBondarchuk <bondarchuk.m.y@gmail.com>
2016-10-22 23:57:43 +0300

    Algorithm base

--- Self-organizing map/Algorithm.cs
+++ Self-organizing map/Algorithm.cs
@@ -23,11 +23,43 @@ namespace Self_organizing_map
                     vector[i] /= vectorSum;
             }
 
+            // Algorithm
+            const double η0 = .5;
+            const double σ0 = .5;
             for (var iter = 0; iter < iterationsNumber; iter++)
             {
+                var exp = Math.Exp(-(double)iter / iterationsNumber);
+                var η = η0 * exp;
+                var σ = σ0 * exp;
+
                 foreach (var vector in inputVectors)
                 {
-                    
+                    var minDist = double.MaxValue;
+                    var minNeuronIdx = -1;
+                    for (var n = 0; n < Map.Neurons.Count; n++)
+                    {
+                        var dist = Map.Neurons[n].DistanceToVector(vector);
+                        if (dist < minDist)
+                        {
+                            minDist = dist;
+                            minNeuronIdx = n;
+                        }
+                    }
+
+                    for (var i = 0; i < Map.Neurons[minNeuronIdx].W.Count; i++)
+                        Map.Neurons[minNeuronIdx].W[i] += η * minDist;
+
+                    foreach (var neuron in Map.Neurons)
+                    {
+                        if (neuron == Map.Neurons[minNeuronIdx])
+                            continue;
+
+                        for (var i = 0; i < neuron.W.Count; i++)
+                        {
+                            var dist = neuron.DistanceToVector(vector);
+                            neuron.W[i] += η * Math.Exp(-dist * dist / (2 * σ * σ)) * minDist;
+                        }
+                    }
                 }
             }
 
@@ -40,7 +72,7 @@ namespace Self_organizing_map
             {
                 var neuron = new Neuron();
                 for (var v = 0; v < vectorSize; v++)
-                    neuron.W.Add(Random.NextDouble()*Random.Next(10));
+                    neuron.W.Add(Random.NextDouble() * Random.Next(10));
                 Map.Neurons.Add(neuron);
             }
         }
--- Self-organizing map/Neuron.cs
+++ Self-organizing map/Neuron.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Self_organizing_map
 {
@@ -8,5 +10,11 @@ namespace Self_organizing_map
         /// Weights
         /// </summary>
         public List<double> W { get; set; } = new List<double>();
+
+        public double DistanceToVector(List<double> vector)
+        {
+            return Math.Sqrt(W.Select((t, i) => (t - vector[i])*(t - vector[i])).Sum());
+        }
+
     }
 }
