02d9b8af9fb126d4d80c74b0259292e312439f61
MaxBondarchuk <bondarchuk.m.y@gmail.com>
2016-11-30 20:33:02 +0200

    Fixed merge

--- Self-organizing map/Algorithm.cs
+++ Self-organizing map/Algorithm.cs
@@ -4,121 +4,123 @@ using System.Linq;
 
 namespace Self_organizing_map
 {
-	public class Algorithm
-	{
-		private Map Map { get; } = new Map();
+    public class Algorithm
+    {
+        private Map Map { get; } = new Map();
 
-		private Random Random { get; } = new Random();
+        private Random Random { get; } = new Random();
 
-		private List<List<double>> InputVectors { get; set; } = new List<List<double>>();
+        private List<List<double>> InputVectors { get; set; } = new List<List<double>>();
 
-		public void Run(List<List<double>> inputVectors, int neuronsNumber) {
-			if (inputVectors.Count == 0)
-				return;
+        public void Run(List<List<double>> inputVectors, int neuronsNumber)
+        {
+            if (inputVectors.Count == 0)
+                return;
 
-			Initialize(inputVectors, inputVectors.First().Count, neuronsNumber);
-			Train();
-			Check();
-		}
+            Initialize(inputVectors, inputVectors.First().Count, neuronsNumber);
+            Train();
+            Check();
+        }
 
-		private void Train() {
-			PrintWeights("Weights before training:");
+        private void Train()
+        {
+            PrintWeights("Weights before training:");
 
-			// Vectors normalization
-			foreach (var vector in InputVectors) {
-				var vectorSum = vector.Sum();
-				for (var i = 0; i < vector.Count; i++)
-					vector[i] /= vectorSum;
-			}
+            // Vectors normalization
+            foreach (var vector in InputVectors)
+            {
+                var vectorSum = vector.Sum();
+                for (var i = 0; i < vector.Count; i++)
+                    vector[i] /= vectorSum;
+            }
 
-			// Algorithm
-<<<<<<< HEAD
-			const int t1 = 1000;
-			const int t2 = 1000;
-			const double η0 = .6;
-			const double ηMin = .01;
-			const double decayRate = .99;
-			const double σ0 = .5;
-			var iter = 0;
-			var η = 1.0;
-			do {
-=======
-			const double fridge = .01;
-			var iter = 0;
-			var learningRate = .99;
-			const double decayRate = .99;
->>>>>>> c86af2169fb4a511c901702b89f79faf6e937e4c
+            // Algorithm
+            const double fridge = .01;
+            var iter = 0;
+            var learningRate = .99;
+            const double decayRate = .99;
 
-			do {
-				foreach (var vector in InputVectors) {
-					var bmuIdx = GetBestMatchingUnit(vector);
+            do
+            {
+                foreach (var vector in InputVectors)
+                {
+                    var bmuIdx = GetBestMatchingUnit(vector);
 
-					var bestNeuron = Map.Neurons[bmuIdx];
-					for (var i = 0; i < Map.Neurons[bmuIdx].W.Count; i++)
-						bestNeuron.W[i] += learningRate * (vector[i] - bestNeuron.W[i]);
+                    var bestNeuron = Map.Neurons[bmuIdx];
+                    for (var i = 0; i < Map.Neurons[bmuIdx].W.Count; i++)
+                        bestNeuron.W[i] += learningRate * (vector[i] - bestNeuron.W[i]);
 
-					foreach (var neuron in Map.Neurons) {
-						if (neuron == bestNeuron)
-							continue;
+                    foreach (var neuron in Map.Neurons)
+                    {
+                        if (neuron == bestNeuron)
+                            continue;
 
-						var dist = neuron.DistanceToVector(vector);
-						for (var i = 0; i < neuron.W.Count; i++)
-							neuron.W[i] += learningRate * Math.Exp(-dist * dist / (2 * learningRate * learningRate)) * (vector[i] - neuron.W[i]);
-					}
-				}
-				iter++;
-				learningRate *= decayRate;
-			} while (fridge < learningRate);
-			Console.WriteLine($"After {iter} iterations\n");
-		}
+                        var dist = neuron.DistanceToVector(vector);
+                        for (var i = 0; i < neuron.W.Count; i++)
+                            neuron.W[i] += learningRate * Math.Exp(-dist * dist / (2 * learningRate * learningRate)) * (vector[i] - neuron.W[i]);
+                    }
+                }
+                iter++;
+                learningRate *= decayRate;
+            } while (fridge < learningRate);
+            Console.WriteLine($"After {iter} iterations\n");
+        }
 
-		private void Check() {
-			PrintWeights("Weights after training:");
+        private void Check()
+        {
+            PrintWeights("Weights after training:");
 
-			foreach (var vector in InputVectors) {
-				var bmuIdx = GetBestMatchingUnit(vector);
-				foreach (var w in vector)
-					Console.Write($"{w,-6:0.00}");
+            foreach (var vector in InputVectors)
+            {
+                var bmuIdx = GetBestMatchingUnit(vector);
+                foreach (var w in vector)
+                    Console.Write($"{w,-6:0.00}");
 
-				Console.Write($" fits into {bmuIdx,4}");
-				Console.WriteLine();
-			}
-			Console.WriteLine();
-		}
+                Console.Write($" fits into {bmuIdx,4}");
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
 
-		private void Initialize(IEnumerable<List<double>> inputVectors, int vectorSize, int neuronsNumber) {
-			InputVectors = new List<List<double>>(inputVectors);
+        private void Initialize(IEnumerable<List<double>> inputVectors, int vectorSize, int neuronsNumber)
+        {
+            InputVectors = new List<List<double>>(inputVectors);
 
-			for (var n = 0; n < neuronsNumber; n++) {
-				var neuron = new Neuron();
-				for (var v = 0; v < vectorSize; v++)
-					neuron.W.Add(Random.NextDouble());
-				Map.Neurons.Add(neuron);
-			}
-		}
+            for (var n = 0; n < neuronsNumber; n++)
+            {
+                var neuron = new Neuron();
+                for (var v = 0; v < vectorSize; v++)
+                    neuron.W.Add(Random.NextDouble());
+                Map.Neurons.Add(neuron);
+            }
+        }
 
-		private void PrintWeights(string message) {
-			Console.WriteLine(message);
-			foreach (var neuron in Map.Neurons) {
-				foreach (var w in neuron.W)
-					Console.Write($"{w,-6:0.00}");
-				Console.WriteLine();
-			}
-			Console.WriteLine();
-		}
+        private void PrintWeights(string message)
+        {
+            Console.WriteLine(message);
+            foreach (var neuron in Map.Neurons)
+            {
+                foreach (var w in neuron.W)
+                    Console.Write($"{w,-6:0.00}");
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
 
-		private int GetBestMatchingUnit(List<double> vector) {
-			var minDist = double.MaxValue;
-			var minNeuronIdx = -1;
-			for (var n = 0; n < Map.Neurons.Count; n++) {
-				var dist = Map.Neurons[n].DistanceToVector(vector);
-				if (!(dist < minDist))
-					continue;
-				minDist = dist;
-				minNeuronIdx = n;
-			}
+        private int GetBestMatchingUnit(List<double> vector)
+        {
+            var minDist = double.MaxValue;
+            var minNeuronIdx = -1;
+            for (var n = 0; n < Map.Neurons.Count; n++)
+            {
+                var dist = Map.Neurons[n].DistanceToVector(vector);
+                if (!(dist < minDist))
+                    continue;
+                minDist = dist;
+                minNeuronIdx = n;
+            }
 
-			return minNeuronIdx;
-		}
-	}
+            return minNeuronIdx;
+        }
+    }
 }
